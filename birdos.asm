cbeed59adef0b5ca08845f57f7c351a7daee9198
lvntky <klevent1903@gmail.com>
2024-07-01 23:28:12 +0300

    [feature] bad square drawing

--- birdos.asm
+++ birdos.asm
@@ -29,7 +29,7 @@ BITS 16
 ORG 0X7C00
 
 %include "constant.asm"
-
+%include "util.mac"
 
 start:
 ;; set 13h mode
@@ -37,24 +37,43 @@ start:
         mov al, 0x13
         int 0x10
 
-        mov cx, 0               ; x-axis
-        mov dx, 0               ; y-axis
-set_background:
+        mov bx, 0
         mov ax, VIDEO_BUFFER
         mov es, ax
-        mov bx, cx
-        imul bx, SCREEN_WIDTH
-        add bx, dx
-        mov BYTE [es:bx], 0xB0
-        inc cx
-        inc dx
 
-        jmp set_background
-
+set_background:
+        mov BYTE [es:bx], BG_COLOR
+        inc bx
+        cmp bx, (SCREEN_WIDTH * SCREEN_HEIGHT)
+        jb set_background
 
 game_loop:
+        call draw_pipe
+        jmp game_loop
 
-    jmp game_loop
+draw_pipe:
+        mov cx, 20        ; Start y-coordinate
+        mov bx, 190       ; End x-coordinate
+        xor dx, dx        ; Initialize x-coordinate to 0
 
+draw_y_axis:
+        mov dx, 20        ; Start x-coordinate for each row
+draw_x_axis:
+        ;set_pixel es, cx, dx, 0x4
+        mov ax, cx
+        imul ax, SCREEN_HEIGHT
+        add ax, dx
+        mov di, ax
+        mov byte [es:di], 0x4
+        inc dx
+        cmp dx, 20
+        jl draw_x_axis    ; Loop until x reaches 190
+
+        inc cx            ; Increment y-coordinate
+        cmp cx, 190        ; This can be adjusted to control height
+        jne draw_y_axis
+
+exit_draw:
+        ret
 times 510 - ($ - $$) db 0   ; Pad the rest of the boot sector with zeros
 dw 0xAA55                   ; Boot sector signature
--- constant.asm
+++ constant.asm
@@ -1,5 +1,12 @@
 BITS 16
 
-SCREEN_WIDTH equ 320
-SCREEN_HEIGHT equ 200
-VIDEO_BUFFER equ 0xA000
+
+        SCREEN_WIDTH equ 320
+        SCREEN_HEIGHT equ 200
+        VIDEO_BUFFER equ 0xA000
+        BG_COLOR equ 0x0B
+        SCREEN_SIZE equ (SCREEN_HEIGHT * SCREEN_WIDTH)
+
+        PIPE_COLOR equ 0xA0
+        PIPE_HEIGHE equ 200
+        PIPE_WIDTH equ 20
